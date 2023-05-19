838fbc5ed3dee21f83555a7f3e93d595ee12aa04
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-05-19 10:40:23 -0300

    creating static header and input

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import Header from "../component/Header";
+
 const Pokemons = () => {
+    const [query, setQuery] = useState("")
     return (
-        <div>
-            Pokemons
-        </div>
+        <>
+            <Header query={query} setQuery={setQuery} />
+           <main>
+            <h1>POKEMONS</h1>
+           </main>
+           <footer>
+            footer
+           </footer>
+        </>
     );
 };
 

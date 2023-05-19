6dbf82521f8f0433b9d75682658dcb5a5487f9d8
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-05-19 18:43:20 -0300

    creating main and pokemon design

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import Header from "../component/Header";
 import Footer from "../component/Footer";
+import { Link } from "react-router-dom";
+import styles from './pokemons.module.css'
+
+import BulbasaurPic from "../assets/bulbasaur.gif"
 
 const Pokemons = () => {
     const [query, setQuery] = useState("")
     return (
         <>
             <Header query={query} setQuery={setQuery} />
-           <main>
-            <h1>POKEMONS</h1>
-           </main>
-           <Footer />
+            <main>
+                <Link className={styles.listeItem} to="/">
+                    <img className={styles.listeItemIcon} src={BulbasaurPic} alt="bulbasaur" />
+                    <div className={styles.listeItemText}>
+                        <span>Bulbasaur</span>
+                        <span>001</span>
+                    </div>
+                </Link>
+            </main>
+            <Footer />
         </>
     );
 };

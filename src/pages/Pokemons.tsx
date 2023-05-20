95d4c5d8180d3fa7d0e752f130c3976f23a8ee17
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-05-20 10:58:58 -0300

    fetch first api

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,24 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 import { Link } from "react-router-dom";
 import styles from './pokemons.module.css'
 
 import BulbasaurPic from "../assets/bulbasaur.gif"
+import { fetchPokemons } from "../api/fetchPokemons";
+import { Pokemon } from "../types/types";
 
 const Pokemons = () => {
     const [query, setQuery] = useState("")
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+        const fetchAllPokemons = async () => {
+            const allPokemons = await fetchPokemons();
+            setPokemons(allPokemons);
+        }
+        fetchAllPokemons()
+    }, [])
+
     return (
         <>
             <Header query={query} setQuery={setQuery} />
             <main>
-                <Link className={styles.listeItem} to="/">
-                    <img className={styles.listeItemIcon} src={BulbasaurPic} alt="bulbasaur" />
-                    <div className={styles.listeItemText}>
-                        <span>Bulbasaur</span>
-                        <span>001</span>
-                    </div>
-                </Link>
+                <nav className={styles.nav}>
+                    {pokemons?.slice(0, 150).map((pokemon) => (
+                        <Link
+                            key={pokemon.id}
+                            className={styles.listeItem}
+                            to={`/pokemons/${pokemon.name.toLocaleLowerCase()}`}
+                        >
+                            <img
+                                className={styles.listeItemIcon}
+                                src={pokemon.imgSrc}
+                                alt={pokemon.name} />
+                            <div className={styles.listeItemText}>
+                                <span>{pokemon.name}</span>
+                                <span>{pokemon.id}</span>
+                            </div>
+                        </Link>
+                    ))}
+                </nav>
             </main>
             <Footer />
         </>

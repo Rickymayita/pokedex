cd74289aa9889ef49b1a622a762050c7af5a7a5a
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-05-22 19:07:18 -0300

    detail page style

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,8 +1,33 @@
-import { useParams} from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import Pokeballimg from '../assets/pokeball.png'
+import Bulbasour from '../assets/bulbasaur.gif'
+import Footer from '../component/Footer';
+import styles from './pokemon.module.css'
 
 const Pokemon = () => {
     const { name } = useParams();
-    return <div>{name}</div>;
+    const navegate = useNavigate();
+
+    return (
+    <>
+        <button className={styles.pokeballbutton} onClick={() => navegate(-1)}>
+           <img className={styles.pokeballImg} src={Pokeballimg} alt="pokeball" /> Go back
+        </button>
+        <div className={styles.pokemon}>
+            <main className={styles.pokemonInfo}>
+                <div className={styles.pokemonTitle}>{name?.toUpperCase()}</div>
+                <div>Nr. 001</div>
+                <div>
+                    <img className={styles.pokemonInfoImg} src={Bulbasour} alt="Bulbasour" />
+                </div>
+                <div>HP: 000</div>
+                <div>Attack: 000</div>
+                <div>Defense: 000</div>
+            </main>
+        </div>
+        <Footer />
+    </>
+    )
 };
 
 export default Pokemon;
